de23e5a1391f8e8f6d2f184f2b96b61e17ffdcac
gionneves <40901802+gionneves@users.noreply.github.com>
2021-03-23 15:54:25 -0300

    update

--- main.py
+++ main.py
@@ -1,13 +1,122 @@
-import view
+import visual.view
+import os
+import random
 
-ver = view.View()
+ver = visual.view.View()
+
+# ---------------------- generator -------------------------
+
+def generator():
+    return random.randint(1, 30)
+    pass
 
 # ---------------------- Logic -------------------------
 
+def gameStart():
+    ver.clear()
+    game = True
+    tipEnable = 1
+    number = generator()
+
+    def tipToss(tip):
+        if tip > 0:
+            if tip == 3:
+                if (number % 2) == 0:
+                    if (number - 4) > 0:
+                        ver.pair_msg(number - 4)
+                    else:
+                        ver.pair_number()
+                else:
+                    if (number + 4) < 30:
+                        ver.odd_msg(number + 4)
+                    else:
+                        ver.odd_number()
+                        pass
+
+            elif tip == 2:
+                if (number - 7) < 0:
+                    x = 0
+                else:
+                    x = number - 7
+                    pass
+
+                if (number + 7) > 30:
+                    y = 30
+                else:
+                    y = (number + 7)
+                    pass
+
+
+                print('The number is between {} and {}'.format(x, y))
+
+            elif tip == 1:
+                for x in range(30):
+                    for y in range(10):
+                        multi = x * y
+                        print(multi)
+                        if multi == number:
+                            print('The number is a multiple of ' + str(y))
+                            break
+            else:
+                print('The number is a prime number')
+
+        else:
+            print("There is no more tips")
+            pass
+        pass
+
+
+###
+# The 'choice_game' funcition is use to send the player to what he choices
+#
+# @param c
+# The param 'c' is use to get the choice of player and what he want to do
+###
+    def choice_game(c):
+        if c == 1:
+            gameStart()
+        elif c == 2:
+            tipToss(tipEnable)
+        elif c == 3:
+            goCredits()
+        else:
+            os.system('exit')
+        pass
+
+    ver.show_msg('Game Start!')
+    print('Try guess the number between 0 and 30 the computer pick up')
+
+    while game:
+        ver.spaceLine()
+        ver.choices('Guess a number', 'Get a tip', 'null', 'null')
+        inp = int(input('Select your choice: '))
+        choice_game(inp)
+        pass
+    pass
+
+###
+# The 'choice_menu' funcition is use to send the player to what he choices
+#
+# @param c
+# The param 'c' is use to get the choice of player and identify where he wanna go
+###
+def choice_menu(c):
+    if c == 1:
+        gameStart()
+    elif c == 2:
+        gameStartDebug()
+    elif c == 3:
+        goCredits()
+    else:
+        os.system('exit')
+    pass
 
 
 # ----------------------- Visual ------------------------
 
-ver.limpar()
-ver.mostrar_mensgem('Wellcome to Guessing game')
-ver.escolhas('Start game', 'Start debug game', 'Credits', 'Exit')
+ver.clear()
+ver.show_msg('Wellcome to Guessing game')
+ver.choices('Start game', 'Start debug game', 'Credits', 'Exit')
+
+inp = int(input('Enter your choice: '))
+choice_menu(inp)
--- view.py
+++ view.py
@@ -1,20 +1,81 @@
+# This was creating by me, Giovanni Neves Sadauscas.
+# There is m github: https://github.com/gionneves
+
 import os
 
 class View:
 
-    def mostrar_mensgem(self, msg):
+###
+#the 'show_msg' function is use to print a message between '#'
+#
+#@param msg
+#gives the function what message will display between the '#'
+###
+    def show_msg(self, msg):
         print('#'*(round(len(msg)/2)*5))
         print(' '*round(len(msg)/2) + msg)
         print('#'*(round(len(msg)/2)*5))
         pass
 
-    def limpar(self):
+###
+# the 'spaceLine' function is use to jump a line, in the middle will put a bar of
+# '#' when display
+###
+    def spaceLine(self):
+        print()
+        print('#'*15)
+        print()
+        pass
+
+###
+# the 'clear' function is auto explaining. He clear the console for the new information
+###
+    def clear(self):
         os.system('cls' if os.name == 'nt' else 'clear')
         pass
 
-    def escolhas(self, a, b, c, d):
-        print('1 - ' + a)
-        print('2 - ' + b)
-        print('3 - ' + c)
-        print('4 - ' + d)
+###
+# The 'choices' function display the choices when the params is definy
+#
+# @param a, b, c, d
+# They has the same objective to show the text inside him but display in order, like
+# a = 1, b = 2, c = 3, d = 4.
+#
+# If one of this param has a string 'null' they will not display
+###
+    def choices(self, a, b, c, d):
+        if a != 'null':
+            print('1 - ' + a)
+        else:
+            pass
+        if b != 'null':
+            print('2 - ' + b)
+        else:
+            pass
+        if c != 'null':
+            print('3 - ' + c)
+        else:
+            pass
+        if d != 'null':
+            print('4 - ' + d)
+        else:
+            pass
+        pass
+
+# ------------------------- game is on ------------------------
+
+    def odd_msg(self, number_divides):
+        print('My number is ODD, try think something close ' + str(number_divides))
+        pass
+
+    def odd_number(self):
+        print('My number is ODD')
+        pass
+
+    def pair_msg(self, number_divides):
+        print('My number is PAIR, try think something close ' + str(number_divides))
+        pass
+
+    def pair_number(self):
+        print('My number is PAIR')
         pass
